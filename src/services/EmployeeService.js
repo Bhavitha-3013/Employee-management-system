a87492ef350f35bedc3bbf732cb5ad6f079b6cb4
Bhavitha <pandalabhavitha@gmail.com>
2026-08-05 14:03:29 +0530

    Update backend API URL for production

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,30 +1,29 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL =
+    "https://employee-management-backend-5mky.onrender.com/api/v1/employees";
 
 class EmployeeService {
 
-    getEmployees() {
+    getEmployees(){
         return axios.get(EMPLOYEE_API_BASE_URL);
     }
 
-    createEmployee(employee) {
+    createEmployee(employee){
         return axios.post(EMPLOYEE_API_BASE_URL, employee);
     }
 
-    getEmployeeById(employeeId) {
+    getEmployeeById(employeeId){
         return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
     }
 
-    updateEmployee(employee, employeeId) {
+    updateEmployee(employee, employeeId){
         return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
     }
 
-    deleteEmployee(employeeId) {
+    deleteEmployee(employeeId){
         return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
     }
 }
 
-const employeeService = new EmployeeService();
-
-export default employeeService;
+export default new EmployeeService();
