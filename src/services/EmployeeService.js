5533cc2b20875bdf21b2b2bc62a97b3a3f9e349a
Bhavitha <pandalabhavitha@gmail.com>
2026-08-06 00:05:02 +0530

    Fix production API URL

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 
 const EMPLOYEE_API_BASE_URL =
-    "https://employee-management-backend-5mky.onrender.com/api/v1/employees";
+  "https://employee-management-backend-5mky.onrender.com/api/v1/employees";
 
 class EmployeeService {
 
-    getEmployees(){
+    getEmployees() {
         return axios.get(EMPLOYEE_API_BASE_URL);
     }
 
-    createEmployee(employee){
+    createEmployee(employee) {
         return axios.post(EMPLOYEE_API_BASE_URL, employee);
     }
 
-    getEmployeeById(employeeId){
-        return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    getEmployeeById(employeeId) {
+        return axios.get(`${EMPLOYEE_API_BASE_URL}/${employeeId}`);
     }
 
-    updateEmployee(employee, employeeId){
-        return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+    updateEmployee(employee, employeeId) {
+        return axios.put(`${EMPLOYEE_API_BASE_URL}/${employeeId}`, employee);
     }
 
-    deleteEmployee(employeeId){
-        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    deleteEmployee(employeeId) {
+        return axios.delete(`${EMPLOYEE_API_BASE_URL}/${employeeId}`);
     }
 }
 
